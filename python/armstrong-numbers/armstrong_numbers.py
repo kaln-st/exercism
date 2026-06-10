a3da77d547c65dc7e9fdcb99085458bcc26428e4
kaln <khanhln.study@gmail.com>
2026-06-10 17:04:42 +0700

    [SOLU][PY004] Armstrong Numbers

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,34 @@
 def is_armstrong_number(number):
-    pass
+    
+    """
+    
+    Write some code to determine whether a number is an Armstrong number.
+
+    Parameter:
+        number (int): positive number to check
+    
+    Returns:
+        bool: is armstrong number or not   
+    
+    """
+
+    # 1. Check number is int or not
+    if type(number) != int: raise ValueError("Input should be a number!")
+
+    # 2. Check number < 0
+    if number < 0: raise ValueError("Number should be a positive number!")
+
+    # 3. Find the exponent
+    numbers = [] # Array of numbers split from number parameter
+    for num in str(number):
+        numbers.append(int(num))
+
+    exponent = len(numbers)
+
+    # 4. Calculate opposite of armstrong number
+    temp_number = 0
+    for num in numbers:
+        temp_number += num ** exponent
+
+    # 5. Check if number is armstrong number or not
+    return number == temp_number
