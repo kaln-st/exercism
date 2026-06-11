043f6e35b4f3cdd75657f4f8f3bb7226f2c8a65d
kaln <khanhln.study@gmail.com>
2026-06-11 16:52:41 +0700

    [SOLU][PY005] Collatz Conjecture

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,27 @@
 def steps(number):
-    pass
+    
+    """
+    
+    Given a positive integer, return the number of steps it takes to reach 1 according to the rules of the Collatz Conjecture.
+
+    Parameters:
+        number (int): positive number
+    
+    Return:
+        int: the number of steps
+
+    """
+
+    if type(number) != int: raise ValueError("Input should be a number!")
+
+    if number <= 0: raise ValueError("Only positive integers are allowed")
+
+    steps = 0
+    while number > 1:
+        if number % 2 == 0:
+            number = number // 2
+        else:
+            number = number * 3 +1
+        steps += 1
+    
+    return steps
