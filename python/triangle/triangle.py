812ae7ec620e778219b790d0fd8512269ad03ea0
khanln <khanhln.study@gmail.com>
2026-06-18 16:54:25 +0700

    [SOLU][PY008] Triangle

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,10 +1,41 @@
-def equilateral(sides):
-    pass
+def is_valid_triangle(sides):
 
+    if len(sides) == 3:
+        a, b, c = sides
+        if a > 0 and b > 0 and c > 0:
+            if (a + b > c) and (a + c > b) and (b + c > a):
+                return True
+        return False
+    else:
+        raise ValueError("Sides must be greater than 0 and have 3 elements")
+
+def equilateral(sides):
+    
+    if is_valid_triangle(sides):
+        a, b, c = sides
+
+        if a == b == c:
+            return True
+        
+    return False
 
 def isosceles(sides):
-    pass
+
+    if is_valid_triangle(sides):
+        a, b, c = sides
+
+        if (a == b and b == a) or (a == c and c == a) or (b == c and c == b):
+            return True
+    
+    return False
 
 
 def scalene(sides):
-    pass
+
+    if is_valid_triangle(sides):
+        a, b, c = sides
+
+        if a != b and a != c and b != c:
+            return True
+    
+    return False
