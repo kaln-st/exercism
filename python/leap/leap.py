6796e1b86ad1883b25a76df7b7c43c79e269abd7
khanln <khanhln.study@gmail.com>
2026-06-22 09:36:38 +0700

    [REFT][PY009] Leap

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -3,7 +3,4 @@ def leap_year(year):
     if type(year) != int:
         raise ValueError("Only positive year will be accept!")
     
-    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-        return True
-    else:
-        return False
+    return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
