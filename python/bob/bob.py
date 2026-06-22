4ffff55dfa0acf7c0c1f5d8cdee7d9053f96fc95
khanln <khanhln.study@gmail.com>
2026-06-22 15:29:00 +0700

    [SOLU][PY011] Bob

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,2 +1,14 @@
 def response(hey_bob):
-    pass
+
+    hey_bob = hey_bob.strip()
+
+    if hey_bob.endswith("?"):
+        if hey_bob.isupper():
+            return "Calm down, I know what I'm doing!"
+        return "Sure."
+    elif hey_bob.isupper():
+        return "Whoa, chill out!"
+    elif not hey_bob:
+        return "Fine. Be that way!"
+    else:
+        return "Whatever."
