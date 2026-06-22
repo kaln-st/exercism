9d7e94750a6c2d863110bee85876468a264ee22c
khanln <khanhln.study@gmail.com>
2026-06-22 16:36:49 +0700

    [SOLU][PY012] Raindrops

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,17 @@
 def convert(number):
-    pass
+    
+    result = ""
+
+    if number % 3 != 0 and number % 5 != 0 and number % 7 != 0:
+        return str(number)
+    
+    if number % 3 == 0:
+        result += "Pling"
+
+    if number % 5 == 0:
+        result += "Plang"
+        
+    if number % 7 == 0:
+        result += "Plong"    
+
+    return result
